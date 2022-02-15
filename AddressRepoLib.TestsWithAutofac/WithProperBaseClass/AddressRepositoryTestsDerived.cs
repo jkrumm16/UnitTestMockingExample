882030286c2a642f900d584d14499f2f9e460218
jkrumm16 <jochen@jkrumm.de>
2022-02-15 22:37:57 +0100

    Added a base and a derived test with autofac
    Added a base and a derived test with autofac

---
+++ AddressRepoLib.TestsWithAutofac/WithProperBaseClass/AddressRepositoryTestsDerived.cs
@@ -0,0 +1,62 @@
+﻿using Autofac;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace AddressRepoLib.TestsWithAutofac.WithProperBaseClass
+{
+    [TestClass]
+    public class AddressRepositoryTestsDerived : MyTestBase<IAddressRepository>
+    {
+        protected override void Arrange(ContainerBuilder containerBuilder)
+        {
+            Builder.RegisterType<AddressRepository>().As<IAddressRepository>();
+            Builder.RegisterType<JsonAddressParser>().As<IAddressParser>();
+
+            var dataSourceMock = new Mock<IAddressDataSource>();
+            dataSourceMock.Setup(x => x.GetAllAddresses()).Returns(new string[]
+            {
+@"{
+    ""LastName"":""Mustermann"",
+    ""FirstName"":""Max"",
+    ""Steet"":""Musterstraße"",
+    ""HouseNumber"":""1a"",
+    ""PostalCode"":""12345"",
+    ""City"":""Musterhausen"",
+    ""ISOA2CountryCode"":""DE""
+}",
+@"{
+    ""LastName"":""Mustermann"",
+    ""FirstName"":""Erika"",
+    ""Steet"":""Musterstraße"",
+    ""HouseNumber"":""1a"",
+    ""PostalCode"":""12345"",
+    ""City"":""Musterhausen"",
+    ""ISOA2CountryCode"":""DE""
+}"
+            });
+
+            InjectMockedDependency(dataSourceMock);
+        }
+
+        [TestMethod]
+        public void AddressRepository_GetAllAddresses_AsJson_ShouldGetTwoAddressCorrect()
+        {
+            ActAndAssert((testScope) =>
+            {
+                ////////////////////////////////////////////////////////////////
+                /// ARRANGE
+                var repository = testScope.Resolve<IAddressRepository>();
+
+                ////////////////////////////////////////////////////////////////
+                /// ACT
+                var addresses = repository.GetAllAddresses();
+
+                ////////////////////////////////////////////////////////////////
+                /// ASSERT
+                Assert.AreEqual(2, addresses.Length);
+                Assert.AreEqual("Max", addresses[0].FirstName);
+                Assert.AreEqual("Erika", addresses[1].FirstName);
+            });
+        }
+    }
+}
---
+++ AddressRepoLib.TestsWithAutofac/WithProperBaseClass/MyTestBase.cs
@@ -0,0 +1,53 @@
+﻿using Autofac;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+
+namespace AddressRepoLib.TestsWithAutofac.WithProperBaseClass
+{
+    public class MyTestBase<TServiceUnderTest>
+    {
+        protected ContainerBuilder Builder { get; private set; }
+        protected List<object> TestDependencies { get; private set; }
+        protected TServiceUnderTest ServiceUnderTest { get; private set; }
+        protected Exception ExceptionThrownInTest { get; private set; }
+
+        [TestInitialize]
+        public void OnInitialize()
+        {
+            Builder = new ContainerBuilder();
+            TestDependencies = new List<object>();
+            Arrange(Builder);
+
+            foreach (var obj in TestDependencies)
+            {
+                Builder.RegisterInstance(obj);
+            }
+        }
+
+        protected virtual void Arrange(ContainerBuilder containerBuilder)
+        {
+        }
+
+        protected virtual void InjectMockedDependency<T>(Mock<T> dependency) where T : class
+        {
+            Builder.RegisterInstance(dependency.Object);
+        }
+
+        protected void ActAndAssert(Action<ILifetimeScope> a)
+        {
+            using (var scope = Builder.Build().BeginLifetimeScope())
+            {
+                try
+                {
+                    a(scope);
+                }
+                catch ( Exception ex ) 
+                {
+                    ExceptionThrownInTest = ex;
+                }
+            }
+        }
+    }
+}
